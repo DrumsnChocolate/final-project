82c086a82325ef5501b9479ce5f32b8882485908
DrumsnChocolate <matbronk@gmail.com>
2023-09-11 20:48:02 +0200

    change env_setup such that it installs the proper packages

--- implementation/visual_prompt_tuning/env_setup.sh
+++ implementation/visual_prompt_tuning/env_setup.sh
@@ -1,9 +1,11 @@
 conda create -n prompt python=3.7
 conda activate prompt
 
-pip install -q tensorflow
+pip install tensorflow
 # specifying tfds versions is important to reproduce our results
-pip install tfds-nightly==4.4.0.dev202201080107
+#pip install tfds-nightly==4.4.0.dev202201080107
+# added by Matteo: ignoring tfds-nightly version because we're having issues with checksums
+pip install tensorflow-datasets
 pip install opencv-python
 pip install tensorflow-addons
 pip install mock
@@ -16,6 +18,7 @@ https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
 pip install opencv-python
 
 conda install tqdm pandas matplotlib seaborn scikit-learn scipy simplejson termcolor
+conda install tqdm pandas matplotlib seaborn scikit-learn scipy simplejson termcolor jupyter
 conda install -c iopath iopath
 
 
@@ -23,6 +26,4 @@ conda install -c iopath iopath
 pip install timm==0.4.12
 pip install ml-collections
 
-# Optional: for slurm jobs
-pip install submitit -U
-pip install slurm_gpustat
+pip install chardet
