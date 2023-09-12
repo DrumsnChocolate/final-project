7d2414b6d4c112e8c815ecf3f16b87cdd22c5acf
DrumsnChocolate <matbronk@gmail.com>
2023-09-12 16:05:32 +0200

    add env_setup.sh for total project

---
+++ env_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sh implementation/visual_prompt_tuning/env_setup.sh
+
+mkdir -p implementation/visual_prompt_tuning/data_path  # folder to put any datasets for vpt, symlinks are also fine
+mkdir -p implementation/visual_prompt_tuning/model_root  # folder to put model roots for vpt. explained in visual_prompt_tuning/README.md
+mkdir -p implementation/visual_prompt_tuning/output_dir  # output directory for vpt
+
+
+
