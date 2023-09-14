47d70e4ce880306fe981a2439e380eb6361f5f43
DrumsnChocolate <matbronk@gmail.com>
2023-09-14 14:16:15 +0200

    define sbatch script that might work

--- implementation/visual_prompt_tuning/experiments/vit_vtab.sh
+++ implementation/visual_prompt_tuning/experiments/vit_vtab.sh
@@ -1,21 +1,36 @@
+conda activate prompt
+
 model_root=model_root
 data_path=data_path
 output_dir=output_dir
-for seed in "42" "44" "82" "100" "800"; do
-  python train.py \
-        --config-file configs/prompt/cub.yaml \
-        MODEL.TYPE "vit" \
-        DATA.BATCH_SIZE "8" \
-        MODEL.PROMPT.NUM_TOKENS "100" \
-        MODEL.PROMPT.DEEP "True" \
-        MODEL.PROMPT.DROPOUT "0.1" \
-        DATA.FEATURE "sup_vitb16_imagenet21k" \
-        DATA.NAME "vtab-dmlab" \
-        DATA.NUMBER_CLASSES "6" \
-        SOLVER.BASE_LR "0.25" \
-        SOLVER.WEIGHT_DECAY "0.001" \
-        SEED ${seed} \
-        MODEL.MODEL_ROOT "${model_root}" \
-        DATA.DATAPATH "${data_path}" \
-        OUTPUT_DIR "${output_dir}/seed${seed}"
+
+# todo: include last dataset after manual download
+#for d in "caltech101 102" "cifar(num_classes=100) 100" "dtd 47" "oxford_flowers102 102" "oxford_iiit_pet 37" "patch_camelyon 2" "sun397 397" "svhn 10" "resisc45 45" "eurosat 10" "dmlab 6" "kitti(task=\"closest_vehicle_distance\") 4" "smallnorb(predicted_attribute=\"label_azimuth\") 18" "smallnorb(predicted_attribute=\"label_elevation\") 9" "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16" "dsprites(predicted_attribute=\"label_orientation\",num_classes=16) 16" "clevr(task=\"closest_object_distance\") 6" "clevr(task=\"count_all\") 8" "diabetic_retinopathy(config=\"btgraham-300\") 5"; do
+# todo: first test for one dataset, and then proceed to the rest
+#for d in "caltech101 102" "cifar(num_classes=100) 100" "dtd 47" "oxford_flowers102 102" "oxford_iiit_pet 37" "patch_camelyon 2" "sun397 397" "svhn 10" "resisc45 45" "eurosat 10" "dmlab 6" "kitti(task=\"closest_vehicle_distance\") 4" "smallnorb(predicted_attribute=\"label_azimuth\") 18" "smallnorb(predicted_attribute=\"label_elevation\") 9" "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16" "dsprites(predicted_attribute=\"label_orientation\",num_classes=16) 16" "clevr(task=\"closest_object_distance\") 6" "clevr(task=\"count_all\") 8"; do
+for d in  "caltech101 102"; do
+  set -- $d
+  dataset=$1
+  num_classes=$2
+
+  # parameters are specified based on the visual prompt tuning paper, and the hyper parameter tuning that was done there
+  for seed in "42" "44" "82" "100" "800"; do
+    python train.py \
+      --config-file configs/prompt/cub.yaml
+      MODEL.TYPE "vit" \
+      DATA.BATCH_SIZE "64" \
+      MODEL.PROMPT.NUM_TOKENS "100" \
+      MODEL.PROMPT.DEEP "True" \
+      MODEL.PROMPT.DROPOUT "0.1" \
+      DATA.FEATURE "sup_vitb16_imagenet21k" \
+      DATA.NAME "vtab-${dataset}" \
+      DATA.NUMBER_CLASSES "${num_classes}" \
+      SOLVER.BASE_LR "5.0" \
+      SOLVER.WEIGHT_DECAY "0.0001" \
+      MODEL.MODEL_ROOT "${model_root}" \
+      DATA.DATAPATH "${data_path}" \
+      OUTPUT_DIR "${output_dir}/seed${seed}"
+  done
 done
+
+conda deactivate
---
+++ slurm/README.md
@@ -0,0 +1,1 @@
+execute all slurm scripts in this folder from the root of the project with `sbatch slurm/<directory>/<filename>.sbatch`
