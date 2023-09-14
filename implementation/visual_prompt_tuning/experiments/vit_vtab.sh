257db1ccb3c31fc4a8c805c141031093dcc9495e
DrumsnChocolate <matbronk@gmail.com>
2023-09-14 16:47:49 +0200

    simplify scripts so much that it will probably be slow but we do not really care currently

--- implementation/visual_prompt_tuning/experiments/vit_vtab.sh
+++ implementation/visual_prompt_tuning/experiments/vit_vtab.sh
@@ -1,8 +1,6 @@
-conda activate prompt
-
-model_root=model_root
-data_path=data_path
-output_dir=output_dir
+model_root=visual_prompt_tuning/model_root
+data_path=visual_prompt_tuning/data_path
+output_dir=visual_prompt_tuning/output_dir
 
 # todo: include last dataset after manual download
 #for d in "caltech101 102" "cifar(num_classes=100) 100" "dtd 47" "oxford_flowers102 102" "oxford_iiit_pet 37" "patch_camelyon 2" "sun397 397" "svhn 10" "resisc45 45" "eurosat 10" "dmlab 6" "kitti(task=\"closest_vehicle_distance\") 4" "smallnorb(predicted_attribute=\"label_azimuth\") 18" "smallnorb(predicted_attribute=\"label_elevation\") 9" "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16" "dsprites(predicted_attribute=\"label_orientation\",num_classes=16) 16" "clevr(task=\"closest_object_distance\") 6" "clevr(task=\"count_all\") 8" "diabetic_retinopathy(config=\"btgraham-300\") 5"; do
@@ -16,7 +14,7 @@ for d in  "caltech101 102"; do
   # parameters are specified based on the visual prompt tuning paper, and the hyper parameter tuning that was done there
   for seed in "42" "44" "82" "100" "800"; do
     python visual_prompt_tuning/train.py \
-      --config-file configs/prompt/cub.yaml
+      --config-file visual_prompt_tuning/configs/prompt/cub.yaml \
       MODEL.TYPE "vit" \
       DATA.BATCH_SIZE "64" \
       MODEL.PROMPT.NUM_TOKENS "100" \
@@ -32,5 +30,3 @@ for d in  "caltech101 102"; do
       OUTPUT_DIR "${output_dir}/seed${seed}"
   done
 done
-
-conda deactivate
