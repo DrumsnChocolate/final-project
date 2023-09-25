10e3aed7decc76d0f922b5a3e65f5aac54ffb8e2
DrumsnChocolate <matbronk@gmail.com>
2023-09-25 22:15:09 +0200

    the rest of the datasets

--- slurm/visual_prompt_tuning/all_vit_vtab.sh
+++ slurm/visual_prompt_tuning/all_vit_vtab.sh
@@ -5,7 +5,7 @@ command="visual_prompt_tuning/experiments/vit_vtab.sh"
 # todo: first test for one dataset, and then proceed to the rest
 #for d in "cifar(num_classes=100) 100" "dtd 47" "oxford_flowers102 102" "oxford_iiit_pet 37" "patch_camelyon 2" "sun397 397" "svhn 10" "dmlab 6" "kitti(task=\"closest_vehicle_distance\") 4" "smallnorb(predicted_attribute=\"label_elevation\") 9" "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16" "dsprites(predicted_attribute=\"label_orientation\",num_classes=16) 16" "clevr(task=\"closest_object_distance\") 6" "clevr(task=\"count_all\") 8" "diabetic_retinopathy(config=\"btgraham-300\") 5"; do
 #for d in "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16"; do
-for d in "oxford_iiit_pet 37" "patch_camelyon 2" "sun397 397"; do
+for d in "svhn 10" "resisc45 45" "eurosat 10" "dmlab 6" "kitti(task=\"closest_vehicle_distance\") 4" "smallnorb(predicted_attribute=\"label_azimuth\") 18" "smallnorb(predicted_attribute=\"label_elevation\") 9" "dsprites(predicted_attribute=\"label_x_position\",num_classes=16) 16" "dsprites(predicted_attribute=\"label_orientation\",num_classes=16) 16" "clevr(task=\"closest_object_distance\") 6" "clevr(task=\"count_all\") 8" "diabetic_retinopathy(config=\"btgraham-300\") 5"; do
   set -- $d
   dataset=$1
   num_classes=$2
