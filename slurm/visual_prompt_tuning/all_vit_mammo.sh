c4ea5c6c5bc9fcfe8d2f0af854f92c6a9da01ba7
DrumsnChocolate <matbronk@gmail.com>
2023-10-22 13:37:02 +0200

    run full finetune with more gpus

--- slurm/visual_prompt_tuning/all_vit_mammo.sh
+++ slurm/visual_prompt_tuning/all_vit_mammo.sh
@@ -4,5 +4,9 @@ for d in "cbis 2 finetune"; do
   dataset=$1
   num_classes=$2
   transfer_type=$3
-  sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type}
+  if [ ${transfer_type} == "finetune" ]; then
+    sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} --gres=gpu:ampere:2
+  else
+    sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type}
+  fi
 done
