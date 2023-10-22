6bd555dd406448595868228ca73e8723716fa5dc
DrumsnChocolate <matbronk@gmail.com>
2023-10-22 14:14:06 +0200

    put some constraints on stuff

--- slurm/visual_prompt_tuning/all_vit_mammo.sh
+++ slurm/visual_prompt_tuning/all_vit_mammo.sh
@@ -5,7 +5,7 @@ for d in "cbis 2 finetune"; do
   num_classes=$2
   transfer_type=$3
   if [ ${transfer_type} == "finetune" ]; then
-    sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} --gres=gpu:ampere:2
+    sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} --gres=gpu:lovelace:1 --constraint=rtx-6000
   else
     sbatch -J "vpt-vit-mammo-${dataset}" slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type}
   fi
