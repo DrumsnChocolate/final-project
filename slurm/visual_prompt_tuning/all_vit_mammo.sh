fad6cdf9dafc7731f1216994cc04a4811716b999
DrumsnChocolate <matbronk@gmail.com>
2023-11-06 17:35:33 +0100

    try setting cuda visible devices

--- slurm/visual_prompt_tuning/all_vit_mammo.sh
+++ slurm/visual_prompt_tuning/all_vit_mammo.sh
@@ -8,7 +8,7 @@ for d in "cbis 2 finetune 224 1"; do
   crop_size=$4
   batch_size=$5
   if [ ${transfer_type} == "finetune" ]; then
-    sbatch -J "vpt-vit-mammo-${dataset}" --constraint=rtx-6000 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
+    CUDA_VISIBLE_DEVICES=0,1,2,3 sbatch -J "vpt-vit-mammo-${dataset}" --constraint=rtx-6000 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
   else
     sbatch -J "vpt-vit-mammo-${dataset}" --gres=gpu:ampere:1 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
   fi
