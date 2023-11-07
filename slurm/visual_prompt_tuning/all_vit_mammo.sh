1e67dc9b87cbffe75d5e81442008fa65e505cedf
DrumsnChocolate <matbronk@gmail.com>
2023-11-07 09:15:16 +0100

    change job name and batch size

--- slurm/visual_prompt_tuning/all_vit_mammo.sh
+++ slurm/visual_prompt_tuning/all_vit_mammo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #for d in "cbis 2 prompt 224 64" "cbis 2 prompt 384 16" "cbis 2 prompt 672 4" "cbis 2 prompt 896 2"; do
-for d in "cbis 2 finetune 224 1"; do
+for d in "cbis 2 finetune 224 64"; do
   set -- $d
   dataset=$1
   num_classes=$2
@@ -8,7 +8,7 @@ for d in "cbis 2 finetune 224 1"; do
   crop_size=$4
   batch_size=$5
   if [ ${transfer_type} == "finetune" ]; then
-    CUDA_VISIBLE_DEVICES=0 sbatch -J "vpt-vit-mammo-${dataset}" --constraint=rtx-6000 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
+    CUDA_VISIBLE_DEVICES=0 sbatch -J "full-vit-mammo-${dataset}" --constraint=rtx-6000 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
   else
     sbatch -J "vpt-vit-mammo-${dataset}" --gres=gpu:ampere:1 slurm/visual_prompt_tuning/vit_mammo.sbatch ${dataset} ${num_classes} ${transfer_type} ${crop_size} ${batch_size}
   fi
