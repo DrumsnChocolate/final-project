6e214e1aa7252947b894c521eb137bfec61dc6bb
DrumsnChocolate <matbronk@gmail.com>
2023-11-19 20:49:19 +0100

    add noaux setr config for ade20k to see how that will perform

---
+++ implementation/mmsegmentation/configs/setr/setr_vit-l_pup-noaux_8xb2-160k_ade20k-512x512.py
@@ -0,0 +1,33 @@
+_base_ = [
+    '../_base_/models/setr_pup.py', '../_base_/datasets/ade20k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+num_layers = 24
+model = dict(
+    data_preprocessor=data_preprocessor,
+    pretrained=None,
+    backbone=dict(
+        img_size=(512, 512),
+        drop_rate=0.,
+        init_cfg=dict(
+            type='Pretrained', checkpoint='pretrain/vit_large_p16.pth'),
+        out_indices=num_layers-1,
+        num_layers=num_layers,
+    ),
+    decode_head=dict(num_classes=150, in_index=0),
+    auxiliary_head=[],
+    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(341, 341)),
+)
+
+optimizer = dict(lr=0.001, weight_decay=0.0)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    paramwise_cfg=dict(custom_keys={'head': dict(lr_mult=10.)}))
+# num_gpus: 8 -> batch_size: 16
+train_dataloader = dict(batch_size=2)
+val_dataloader = dict(batch_size=1)
+test_dataloader = val_dataloader
