265f7307e2a3b6aa50f846e43a061f25c1fd3fcb
DrumsnChocolate <matbronk@gmail.com>
2023-12-24 19:25:11 +0100

    sweep setr full cbis-binary

--- slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
@@ -1,3 +1,4 @@
-lr=0.001
-wd=0.0
-sbatch -J "full-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} full "cbis-binary"
+wd=0.001
+for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
+  sbatch -J "full-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} full "cbis-binary"
+done
--- slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
@@ -13,7 +13,7 @@
 #sbatch -J "vpt-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} vpt "cbis-binary"
 
 wd=0.001
-for lr in 0.05 0.01 0.005 0.001 0.0005 0.0001; do
+for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "vpt-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} vpt "cbis-binary"
 done
 
