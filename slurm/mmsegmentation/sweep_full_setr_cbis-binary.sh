6cafda44f193ae64575b426c235b297c29d2b12a
DrumsnChocolate <matbronk@gmail.com>
2024-01-01 13:08:25 +0100

    also apply class balancing to auxheads

--- slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
@@ -1,5 +1,5 @@
 wd=0.001
-balanced=false
+balanced=true
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "full-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} full "cbis-binary" ${balanced}
 done
--- slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
@@ -4,7 +4,7 @@
 
 
 wd=0.001
-balanced=false
+balanced=true
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "vpt-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} vpt "cbis-binary" ${balanced}
 done
