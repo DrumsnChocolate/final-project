d68a8f020872ae768c1882498b5ee23e2c6428b6
DrumsnChocolate <matbronk@gmail.com>
2024-01-02 21:17:45 +0100

    also conduct for non balanced, but with adamw

--- slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
@@ -4,8 +4,8 @@
 
 
 wd=0.001
-balanced=true
-optimizer=sgd
+balanced=false
+optimizer=adamw
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "vpt-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} vpt "cbis-binary" ${balanced} ${optimizer}
 done
