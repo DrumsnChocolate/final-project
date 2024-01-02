8424843db29bd1e1f2c12f0bb97cb1ba6cceaa2f
DrumsnChocolate <matbronk@gmail.com>
2024-01-02 21:12:10 +0100

    also conduct again for sgd, because these failed too

--- slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
@@ -1,6 +1,6 @@
 wd=0.001
 balanced=true
-optimizer=adamw
+optimizer=sgd
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "full-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} full "cbis-binary" ${balanced} ${optimizer}
 done
--- slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_vpt_setr_cbis-binary.sh
@@ -5,7 +5,7 @@
 
 wd=0.001
 balanced=true
-optimizer=adamw
+optimizer=sgd
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "vpt-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} vpt "cbis-binary" ${balanced} ${optimizer}
 done
