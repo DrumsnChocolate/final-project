376a81a5b074de3a821eaf49424c3b4145c53d47
DrumsnChocolate <matbronk@gmail.com>
2024-01-30 17:33:12 +0100

    update two testing configs and the results notebook

--- implementation/mmsegmentation/configs/setr/setr_vit-b-224_pup-noaux_8xb2-160k_cbis-ddsm-binary-224x224.py
+++ implementation/mmsegmentation/configs/setr/setr_vit-b-224_pup-noaux_8xb2-160k_cbis-ddsm-binary-224x224.py
@@ -1,9 +1,9 @@
 _base_ = [
-    '../_base_/models/setr_pup.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/setr_pup.py', '../_base_/datasets/cbis-ddsm-binary.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
-num_layers = 1
-crop_size = (200, 200)
+num_layers = 12
+crop_size = (224, 224)
 data_preprocessor = dict(size=crop_size)
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
@@ -11,14 +11,16 @@ model = dict(
     pretrained=None,
     backbone=dict(
         img_size=crop_size,
+        embed_dims=768,
+        num_heads=12,
         drop_rate=0.,
-        init_cfg=dict(type='Pretrained', checkpoint='pretrain/vit_base_p16_384.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='pretrain/vit_base_p16_224.pth'),
         num_layers=num_layers,
         out_indices=num_layers-1,  # output only last layer
     ),
-    decode_head=dict(num_classes=150, in_index=0),
+    decode_head=dict(num_classes=2, in_index=0, in_channels=768),
     auxiliary_head=[],  # no auxiliary head
-    test_cfg=dict(mode='slide', crop_size=crop_size, stride=(100, 100)),
+    test_cfg=dict(mode='whole'),
 )
 
 optimizer = dict(lr=0.001, weight_decay=0.0)
