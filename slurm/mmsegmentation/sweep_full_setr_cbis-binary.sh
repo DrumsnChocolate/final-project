b3c969cf09dda92cbe28f304210efbaceadadc9d
DrumsnChocolate <matbronk@gmail.com>
2024-01-30 13:41:35 +0100

    configure experiments to investigate the effect of normalization

--- slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-binary.sh
@@ -1,7 +1,7 @@
 wd=0.001
-balanced=true
+balanced=false
 optimizer=sgd
-auxiliary_head=false
+auxiliary_head=true
 for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
   sbatch -J "full-setr-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/setr_cbis.sbatch ${lr} ${wd} full "cbis-binary" ${balanced} ${optimizer} ${auxiliary_head}
 done
