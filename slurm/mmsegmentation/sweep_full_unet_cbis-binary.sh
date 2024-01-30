dfcac25b923f4c7c1d7d3d25eb4f99acda623402
DrumsnChocolate <matbronk@gmail.com>
2024-01-30 20:53:34 +0100

    for now, lets not sweep but just test one lr instead

--- slurm/mmsegmentation/sweep_full_unet_cbis-binary.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-binary.sh
@@ -1,4 +1,6 @@
 wd=0.0005
-for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
-  sbatch -J "full-unet-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${wd}
-done
+lr=0.001
+sbatch -J "full-unet-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${wd}
+#for lr in 0.1 0.05 0.01 0.005 0.001 0.0005 0.0001; do
+#  sbatch -J "full-unet-cbis-binary-lr${lr}_wd${wd}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${wd}
+#done
