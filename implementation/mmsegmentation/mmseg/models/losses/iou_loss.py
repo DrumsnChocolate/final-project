091ebf5486d72c2869f00dc67c06aacf85d33482
DrumsnChocolate <matbronk@gmail.com>
2024-02-07 20:09:38 +0100

    start work on implementing mmseg iou_loss based on connected U-Nets codebase

--- implementation/mmsegmentation/mmseg/models/losses/iou_loss.py
+++ implementation/mmsegmentation/mmseg/models/losses/iou_loss.py
@@ -11,7 +11,10 @@ def iou_loss(pred: torch.Tensor,
              reduction: Union[str, None] = 'mean',
              avg_factor: Union[float, None] = None,
              ignore_index: int = 255) -> float:
-    """Calculate IoU loss, arguments similar to dice loss."""
+    """Calculate IoU loss, arguments similar to dice loss.
+    The precise definition used here is the one from connected-unets:
+    https://github.com/AsmaBaccouche/Connected-Unets-and-more/blob/main/Basic_Unet_segmnetation.py
+    """
     if ignore_index is not None:
         num_classes = pred.shape[1]
         pred = pred[:, torch.arange(num_classes) != ignore_index, :, :]
@@ -19,8 +22,8 @@ def iou_loss(pred: torch.Tensor,
         assert pred.shape[1] != 0 # if the ignored index is the only class
     input = pred.flatten(1)
     target = target.flatten(1).float()
-    a = torch.sum(input * target > 0, 1)
-    b = torch.sum((input > 0) + (target > 0), 1)  # boolean addition is the same as logical or
+    a = torch.abs(torch.sum(input * target))
+    b = torch.sum(input) + torch.sum(target > 0), 1)  # boolean addition is the same as logical or
     iou = a / b
     loss = 1 - iou
     if weight is not None:
