bd6182e3eb44277e2fac0e6e4f905369d25a8db5
DrumsnChocolate <matbronk@gmail.com>
2024-02-14 13:04:59 +0100

    configure script to execute unet with 512 input

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -1,2 +1,3 @@
 lr=0.0001
-sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr}
+size=512
+sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size}
