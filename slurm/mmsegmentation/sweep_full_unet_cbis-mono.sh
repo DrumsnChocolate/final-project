f335f7ac37b8d00ff8ba2b25903ad94e65d8663c
DrumsnChocolate <matbronk@gmail.com>
2024-02-22 21:04:07 +0100

    add option to use binary CE loss in unet

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -1,3 +1,4 @@
 lr=0.0001
 size=256
-sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size}
+loss="cross_entropy"
+sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size} ${loss}
