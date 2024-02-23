e45781ec1e1a561c908715db69cf2ec3e8e7de7a
DrumsnChocolate <matbronk@gmail.com>
2024-02-23 13:49:31 +0100

    do not forgot to normalize differently when using clahe

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -1,4 +1,5 @@
 lr=0.0001
 size=256
 loss="cross_entropy"
-sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size} ${loss}
+clahe="true"
+sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size} ${loss} ${clahe}
