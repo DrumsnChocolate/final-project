afb1cb06266596a11716a960fd0b2a46d3ed4bd8
m.bronkhorst-1@student.utwente.nl <s1952889@hpc-head1.local>
2024-02-28 15:26:30 +0100

    some small fixes to scripts

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -2,4 +2,4 @@ lr=0.0001
 size=256
 loss="cross_entropy"
 clahe="true"
-sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch ${lr} ${size} ${loss} ${clahe}
+sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch "${lr}" "${size}" "${loss}" "${clahe}"
