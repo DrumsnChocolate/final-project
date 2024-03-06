3b76f4bad0ee465f4782b7f9a8c76b7c90c420e5
DrumsnChocolate <matbronk@gmail.com>
2024-03-06 15:47:59 +0100

    update slurm settings to cope with different data location

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -3,4 +3,5 @@ size=256
 loss="cross_entropy"
 clahe="true"
 augmentation="false"
-sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch "${lr}" "${size}" "${loss}" "${clahe}" "${augmentation}"
+mass_only="true"
+sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch "${lr}" "${size}" "${loss}" "${clahe}" "${augmentation}" "${mass_only}"
