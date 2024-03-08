7c0689f327a5476be3b9491657eb9ec69a161016
DrumsnChocolate <matbronk@gmail.com>
2024-03-08 16:01:22 +0100

    update scripts to allow for varying input size for cbis mono setr

--- slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
@@ -1,1 +1,3 @@
-sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch
+lr=0.0001
+size=256
+sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}"
