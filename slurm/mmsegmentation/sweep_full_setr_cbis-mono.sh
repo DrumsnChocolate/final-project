d72e8651f9c776a8917a4fc1caf9b870fe3f0c84
DrumsnChocolate <matbronk@gmail.com>
2024-03-09 12:31:08 +0100

    add pretrained toggle

--- slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
@@ -1,4 +1,5 @@
 lr=0.0001
 size=512
 batch_size=8
-sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}" "${batch_size}"
+pretrained=true
+sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}" "${batch_size}" "${pretrained}"
