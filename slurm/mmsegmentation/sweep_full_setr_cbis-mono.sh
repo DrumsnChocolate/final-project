9bcb3d06b34829640bc5e1847c823a13e1792ba1
DrumsnChocolate <matbronk@gmail.com>
2024-03-09 12:04:18 +0100

    change batch size for 512

--- slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
@@ -1,3 +1,4 @@
 lr=0.0001
 size=512
-sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}"
+batch_size=12
+sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}" "${batch_size}"
