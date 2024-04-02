7c134fe01586492e94d883e4e187bca1b16610ef
DrumsnChocolate <matbronk@gmail.com>
2024-04-02 16:03:11 +0200

    also run for mass only

--- slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_unet_cbis-mono.sh
@@ -3,5 +3,5 @@ size=224
 loss="cross_entropy"
 clahe="true"
 augmentation="false"
-mass_only="false"
+mass_only="true"
 sbatch -J "full-unet-cbis-mono-lr${lr}" slurm/mmsegmentation/unet_cbis.sbatch "${lr}" "${size}" "${loss}" "${clahe}" "${augmentation}" "${mass_only}"
