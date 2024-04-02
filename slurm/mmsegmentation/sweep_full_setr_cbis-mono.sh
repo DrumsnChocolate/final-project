d63b614781ed07eabfeef874dcae3e9937d60f50
DrumsnChocolate <matbronk@gmail.com>
2024-04-02 15:36:12 +0200

    update scripts

--- slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
@@ -1,6 +1,6 @@
 lr=0.0001
-size=384
+size=224
 batch_size=16
 pretrained=true
-loss="iou"
+loss="cross_entropy"
 sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}" "${batch_size}" "${pretrained}" "${loss}"
