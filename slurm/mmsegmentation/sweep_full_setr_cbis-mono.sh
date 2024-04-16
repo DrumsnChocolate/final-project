fb24e3cbc75c0d991a47d091cb82e93b695a9c3a
m.bronkhorst-1@student.utwente.nl <s1952889@hpc-head1.local>
2024-04-16 10:20:52 +0200

    slurm script changes

--- slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
+++ slurm/mmsegmentation/sweep_full_setr_cbis-mono.sh
@@ -1,7 +1,7 @@
 lr=0.0001
 size=224
 batch_size=16
-pretrained=true
+pretrained=false
 loss="cross_entropy"
 tuning_method=full
 sbatch -J "full-setr-cbis-mono" slurm/mmsegmentation/setr_cbis_mono.sbatch "${lr}" "${size}" "${batch_size}" "${pretrained}" "${loss}" "${tuning_method}"
